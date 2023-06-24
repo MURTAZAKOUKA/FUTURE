162c895b936ac21e746fc84e0b60607f3434896e
MURTAZAKOUKA <murtazakoukawala@gmail.com>
2023-06-24 13:53:19 +0500

    app.js have if else work and index2 have bootstrap table work

---
+++ app.js
@@ -0,0 +1,19 @@
+grade=1
+if(grade>=90){
+    console.log("A+")
+} 
+else if(grade>=80){
+    console.log("A")
+}
+else if(grade>=70){
+    console.log("B")
+}
+else if(grade>=60){
+    console.log("C")
+}
+else if(grade>=50){
+    console.log("D")
+}
+else{
+    console.log("F")
+}
